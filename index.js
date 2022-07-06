c654cd0a27a008d986767efbcdf3e9523fe5f7cc
Lucas Lara <lucasml@live.com>
2022-07-06 16:17:03 -0300

    Switch Calculator 1.0

---
+++ index.js
@@ -0,0 +1,23 @@
+function calculator(a,b) {
+  
+  
+  switch(op){
+    case 'sum':
+      a + b
+      break;
+    case 'minus':
+      a - b
+      break;
+    case 'multiply':
+      a * b
+      break;
+    case 'divide':
+      a / b
+      break;
+    case 'module':
+      a % b
+      break;
+    default:
+      console.log('Operation Not Defined')
+  }
+}
