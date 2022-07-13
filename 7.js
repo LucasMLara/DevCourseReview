91ccca6ebfdd079ad4114c8b56af0126e2dfd3ee
Lucas Lara <lucasml@live.com>
2022-07-13 16:40:51 -0300

    Exercise 7

--- 7.js
+++ 7.js
@@ -4,12 +4,23 @@ function setAmericanGrades(grade) {
 
   if(typeof grade !== 'number') {
     return result = 'Insira apenas números'
+  } else if(grade > 100 || grade < 0) {
+    result = 'Erro: Nota inserída inválida'
+  } else if (grade >= 90) {
+    result = 'A'
+  } else if(grade >= 80) {
+    result = 'B'
+  } else if(grade >= 70) {
+    result = 'C'
+  } else if(grade >= 60) {
+    result = 'D'
+  } else if(grade >= 50) {
+    result = 'E'
+  } else {
+    result = 'F'
   }
 
-  if(grade > 100 || grade < 0) {
-    result = 'Erro: Nota inserída inválida'
-  }
   return result;
 }
 
-console.log(setAmericanGrades(-1));
+console.log(setAmericanGrades(90));
