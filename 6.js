ded5091240ac5b6f90c10d0d1e724072b727d617
Lucas Lara <lucasml@live.com>
2022-07-13 10:19:09 -0300

    Exercise 6

---
+++ 6.js
@@ -0,0 +1,32 @@
+function chessPiecesMovements(piece){
+
+  let universalPiece = piece.toLowerCase()
+  
+  let result;
+
+  switch(universalPiece) {
+    case 'king':
+      result = 'King Movements'
+      break;
+    case 'queen':
+      result = 'Queen Movements'
+      break;
+    case 'rook':
+      result = 'Rook Movements'
+      break;
+    case 'bishop':
+      result = 'Bishop Movements'
+      break;
+    case 'knight':
+      result = 'Knight Movements'
+      break;
+    case 'pawn':
+      result = 'Pawn Movements'
+      break;
+    default:
+      result = 'Piece Not found in this game'
+  }
+  return result
+}
+
+console.log(chessPiecesMovements('kindsg'))
