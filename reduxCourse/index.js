70027523ab77a144c17ac75142e336b8415840b7
Lucas Lara <lucasml@live.com>
2022-08-31 14:37:26 -0300

    Colors Array

---
+++ reduxCourse/index.js
@@ -0,0 +1,16 @@
+const Redux = require('redux')
+const fazerLogin = (email) => ({type:'LOGIN', email})
+const INITIAL_STATE = {login: false, email:""}
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    case 'LOGIN':
+      return {...state, login: !state.login, email: action.email}
+      default:
+        return state
+  }
+}
+
+const store = Redux.createStore(reducer)
+store.dispatch(fazerLogin('lucasml@live.com'))
+console.log(store.getState());
